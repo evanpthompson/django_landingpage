089fe18b445fa1558c99a412cdaf3b3f1bc0dcec
Evan Thompson <evanpthompson@gmail.com>
2016-06-28 22:44:47 -0500

    upload for review

---
+++ proj_app/landingapp/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from proj_app.landingapp.models import Document
+admin.site.register(Document)
+
+# Register your models here.
---
+++ proj_app/landingapp/views.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+from proj_app.landingapp.models import Document
+from proj_app.landingapp.forms import DocumentForm
+
+
+
+def list(request):
+    # Handle file upload
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            newdoc = Document(docfile=request.FILES['docfile'])
+            newdoc.save()
+
+            # Redirect to the document list after POST
+            return HttpResponseRedirect(reverse('proj_app.landingapp.views.list'))
+
+    else:
+        form = DocumentForm()  # A empty, unbound form
+
+    # Load documents for the list page
+    documents = Document.objects.all()
+
+    # Render list page with the documents and the form
+    return render_to_response(
+            'list.html',
+            {'documents': documents, 'form': form},
+            context_instance=RequestContext(request))
+
+
+
+
+
+
